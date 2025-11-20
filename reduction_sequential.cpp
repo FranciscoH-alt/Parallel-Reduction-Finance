d8f3e4d347600c770e88b28dc8796e2a844942c1
FranciscoH-alt <147673162+FranciscoH-alt@users.noreply.github.com>
2025-11-20 14:14:54 -0500

    Delete reduction_sequential.cpp

--- reduction_sequential.cpp
+++
@@ -1,9 +0,0 @@
-#include "reduction.hpp"
-
-float reduce_sequential(const std::vector<float>& data) {
-    float sum = 0.0f;
-    for (float x : data) {
-        sum += x;
-    }
-    return sum;
-}
