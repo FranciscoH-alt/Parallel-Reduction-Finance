14fd7d31418a3d18bc66652770e092d67dbaf5e7
FranciscoH-alt <fhenriques@oakland.edu>
2025-11-20 14:12:51 -0500

    Initial push of parallel reduction project

---
+++ build/CMakeFiles/parallel_reduction.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/franciscohenriques/Projects/Parallel-Programming/parallel-reduction/src/main.cpp" "CMakeFiles/parallel_reduction.dir/src/main.cpp.o" "gcc" "CMakeFiles/parallel_reduction.dir/src/main.cpp.o.d"
+  "/Users/franciscohenriques/Projects/Parallel-Programming/parallel-reduction/src/reduction_omp.cpp" "CMakeFiles/parallel_reduction.dir/src/reduction_omp.cpp.o" "gcc" "CMakeFiles/parallel_reduction.dir/src/reduction_omp.cpp.o.d"
+  "/Users/franciscohenriques/Projects/Parallel-Programming/parallel-reduction/src/reduction_sequential.cpp" "CMakeFiles/parallel_reduction.dir/src/reduction_sequential.cpp.o" "gcc" "CMakeFiles/parallel_reduction.dir/src/reduction_sequential.cpp.o.d"
+  "/Users/franciscohenriques/Projects/Parallel-Programming/parallel-reduction/src/reduction_simd.cpp" "CMakeFiles/parallel_reduction.dir/src/reduction_simd.cpp.o" "gcc" "CMakeFiles/parallel_reduction.dir/src/reduction_simd.cpp.o.d"
+  "/Users/franciscohenriques/Projects/Parallel-Programming/parallel-reduction/src/util.cpp" "CMakeFiles/parallel_reduction.dir/src/util.cpp.o" "gcc" "CMakeFiles/parallel_reduction.dir/src/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/parallel_reduction.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_reduction.dir/src/main.cpp.o"
+  "CMakeFiles/parallel_reduction.dir/src/main.cpp.o.d"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_omp.cpp.o"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_omp.cpp.o.d"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_sequential.cpp.o"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_sequential.cpp.o.d"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_simd.cpp.o"
+  "CMakeFiles/parallel_reduction.dir/src/reduction_simd.cpp.o.d"
+  "CMakeFiles/parallel_reduction.dir/src/util.cpp.o"
+  "CMakeFiles/parallel_reduction.dir/src/util.cpp.o.d"
+  "parallel_reduction"
+  "parallel_reduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel_reduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
